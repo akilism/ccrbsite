e0e2f3b18a22bbbb78da72a353d49f2c8a61afc4
Akil Harris <akil.harris@gmail.com>
2014-10-04 11:21:41 -0400

    select shape and neighboring shapes

--- client/app/largeMap/largeMap.directive.js
+++ client/app/largeMap/largeMap.directive.js
@@ -20,19 +20,57 @@ directives.directive('largeMap', function () {
 directives.largeMap = function ($scope, $element, $attrs, $http) {
 
   var mapHelpers = (function() {
-    var highlightShape = function (elem) {
-      elem.classList.add('highlighted-path');
+    var highlightShape = function (evt) {
+      console.log('highlight');
+      evt.target._path.classList.add('highlighted-path');
+      evt.target.bringToFront();
     };
 
     // Remove highlight on mouseout.
-    var removeShapeHighlight = function (elem) {
-      elem.classList.remove('highlighted-path');
+    var removeShapeHighlight = function (evt) {
+      console.log('remove');
+      evt.target._path.classList.remove('highlighted-path');
+    };
+
+    var highlightNeighbors = function (evt) {
+      console.log(evt);
+      var activeStyle = {
+        color: 'rgb(0, 0, 0)',
+        weight: 1.25,
+        fillColor: 'rgb(128, 0, 0)',
+        //fillColor: layer.feature.properties.color,
+        fillOpacity: '0.75'
+      };
+
+      var inactiveStyle = {
+        color: 'rgb(0, 0, 0)',
+        weight: 1.25,
+        fillColor: 'rgb(0, 0, 128)',
+        //fillColor: layer.feature.properties.color,
+        fillOpacity: '0.75'
+      };
+
+      setNeighbors(evt, activeStyle, inactiveStyle);
+    };
+
+    var setNeighbors = function (evt, activeStyle, inactiveStyle) {
+      var elemBounds = evt.target.getBounds();
+
+      $scope.precinctGroup.eachLayer(function (layer) {
+        var layerBounds = layer.getBounds();
+        if (elemBounds.intersects(layerBounds)) {
+          layer.setStyle(activeStyle);
+        } else {
+          layer.setStyle(inactiveStyle);
+        }
+      });
     };
 
     // Set event handlers on shapes.
     var setShape = function (feature, layer) {
-      layer.on('mouseover', highlightShape);
+      layer.on('mousemove', highlightShape);
       layer.on('mouseout', removeShapeHighlight);
+      layer.on('click', highlightNeighbors);
       layer.setStyle({
         color: 'rgb(0, 0, 0)',
         weight: 1.25,
@@ -52,20 +90,20 @@ directives.largeMap = function ($scope, $element, $attrs, $http) {
 
 
   var setMap = function (mapId) {
-    var map = L.map(mapId); //.setView([40.7127, -74.0059], 12);
+    $scope.map = $scope.map || L.map(mapId); //.setView([40.7127, -74.0059], 12);
 
-    var precinctGroup = L.featureGroup();
+    $scope.precinctGroup = $scope.precinctGroup || L.featureGroup();
     _.forEach($scope.shapes.features, function (shape) {
       var geo = L.geoJson(shape, {
           onEachFeature: mapHelpers.setShape
         });
-      precinctGroup.addLayer(geo);
+      $scope.precinctGroup.addLayer(geo);
     });
 
-    var bounds = precinctGroup.getBounds();
+    var bounds = $scope.precinctGroup.getBounds();
 
-    precinctGroup.addTo(map);
-    map.setView(bounds.getCenter(), 11);
+    $scope.precinctGroup.addTo($scope.map);
+    $scope.map.setView(bounds.getCenter(), 11);
 
 
     var tiles = L.tileLayer('http://openmapsurfer.uni-hd.de/tiles/roadsg/x={x}&y={y}&z={z}', {
@@ -74,7 +112,7 @@ directives.largeMap = function ($scope, $element, $attrs, $http) {
       maxZoom: 13
     });
 
-    tiles.addTo(map);
+    tiles.addTo($scope.map);
   };
 
   setMap('largeMap');
